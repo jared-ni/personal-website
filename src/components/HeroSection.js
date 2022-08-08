899a1e435f6490598fedddb5d9990e1e7130aa8e
milkteadj <milkteadj@gmail.com>
2022-08-08 00:53:56 -0400

    finish web view of hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -10,6 +10,20 @@ function HeroSection() {
 
         <h1>Hello World.</h1> 
         <p>My name is Jared Ni.</p>
+        <div className="pp">I am a rising sophomore at Harvard studying computer science and economics.
+            My dream is to engineer software products that improve the way people live.
+            Here are some interesting facts about me: 
+        </div>
+        <div className="pp-points">
+            &emsp; <i class="fa-solid fa-quote-left"></i>        
+                &nbsp; I helped build a college matchmaking app used by 50k students 
+                &nbsp; <i class="fa-solid fa-heart"></i> 
+            <br /> &emsp; <i class="fa-solid fa-quote-left"></i> 
+                &nbsp; I designed and programmed the frontend of a drone-delivery mobile app for a start-up 
+                &nbsp; <i class="fa-solid fa-helicopter"></i>
+            <br /> &emsp; <i class="fa-solid fa-quote-left"></i> 
+                &nbsp; Favorite Harvard course so far: CS20 Discrete Mathematics for CS &nbsp; <i class="fa-solid fa-share-nodes"></i>
+        </div>
     </div>
 
   )
