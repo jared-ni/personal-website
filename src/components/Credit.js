b6f44029158efa2c79f36efbfd313c19422920d6
milkteadj <milkteadj@gmail.com>
2022-08-23 21:12:30 -0400

    fasdfads

--- src/components/Credit.js
+++ src/components/Credit.js
@@ -2,13 +2,9 @@ import React, {useEffect, useState} from "react";
 import '../App.css';
 import './Credit.css';
 
-import axios from "axios";
+
 
 function Credit() {
-    const [datas, setDatas] = useState([{
-        title: 'fdasf',
-        content: 'sfds'
-    }])
 
     useEffect(() => {
         axios.get("/datas").then(response => {
@@ -16,23 +12,6 @@ function Credit() {
             console.log("notes datas: " + datas[0].title);
         });
     })
-    
-
-    return (
-        <div className="credit-container">
-            <div className="credit-center-container">
-                Designed &amp; Created by Jared Ni
-                {console.log("data array: " + datas)}
-                {datas[0].content}
-                {datas.map(data => 
-                    <div>
-                        <h1>asdfsadf</h1>
-                        <p>{data.content}</p>
-                    </div>
-                )}
-            </div>
-        </div>
-    );
 }
 
 export default Credit
