600df2152ddbf36cff27c7e9c794b57c33d511d3
jared-ni <jared.ni@salesforce.com>
2023-08-03 12:13:17 -0700

    try

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -11,26 +11,28 @@ function HeroSection() {
             <img className="heroImage" src={bridgeImage} alt="Jared Ni"  />
         </div>
         <h1 className="hero-h1">Hello World, my name is</h1> 
-        <p>Jared Ni.</p>
-        <div className="pp">I am a first-generation college student studying computer science and economics at Harvard, 
-            and I love building accessible and impactful software products. 
-            Here are some interesting facts about me: 
+        <p>Jared Ni</p>
+        <div className="pp">
+            I am a first-generation college student studying Computer Science, Economics, and Molecular and Cellular Biology at Harvard. 
+            I'm passionate about leveraging technology to improve the lives of others. 
+            Currently, I'm working on: 
         </div>
         <div className="pp-points-container">
             <div className="pp-points"> 
-                I helped build a college matchmaking app used by 50k students 
-                    &nbsp; <i class="fa-solid fa-heart"></i> 
+                Software Engineering project at Salesforce, improving data security for organizations
+                    &nbsp; <i class="fa-solid fa-user-shield"></i>
             </div>
             <div className="pp-points"> 
-                As a SWE intern, I created a Backend Service for fetching, processing, and displaying financial research data
-                    &nbsp; <i class="fa-solid fa-comments-dollar"></i>
-            </div>
-            <div className="pp-points"> 
-                I designed and programmed the frontend of a start-up's healthcare drone-delivery app
-                    &nbsp; <i class="fa-solid fa-helicopter"></i>
+                Machine Learning research at Harvard Edge Computing Lab, creating a reinforcement framework for computer architectural design
+                    &nbsp; <i class="fa-solid fa-user-shield"></i>
             </div>
             <div className="pp-points">
-                Favorite Harvard course so far: CS61: Systems Programming and Machine Organization &nbsp; <i class="fa-solid fa-code"></i>
+                Systems Oncology Research at Mass-General, correlating morphology with treatment resistance in pancreatic cancer cells
+                    &nbsp; <i class="fa-solid fa-dna"></i>
+            </div>
+            <div className="pp-points"> 
+                Teaching CS61: Systems Programming and Machine Organization at Harvard, as Teaching Fellow
+                    &nbsp; <i class="fa-solid fa-computer"></i>
             </div>
         </div>
         </div>
